29361e5b0708090ba8708576102158844b2232aa
TheUnholyPear <ryanbain458@hotmail.com>
2024-10-11 11:29:34 +0100

    Commiting week3 lab to then expand on, and also adding all the relevant .jar files relating to javaParser

---
+++ resources/Library.java
@@ -0,0 +1,88 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+    // Inner class representing a Book
+    class Book {
+        private String title;
+        private String author;
+        private String isbn;
+
+        public Book(String title, String author, String isbn) {
+            this.title = title;
+            this.author = author;
+            this.isbn = isbn;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getAuthor() {
+            return author;
+        }
+
+        public String getIsbn() {
+            return isbn;
+        }
+
+        @Override
+        public String toString() {
+            return "Title: " + title + ", Author: " + author + ", ISBN: " + isbn;
+        }
+    }
+
+    // List to store books in the library
+    private List<Book> books;
+
+    public Library() {
+        books = new ArrayList<>();
+    }
+
+    // Method to add a book to the library
+    public void addBook(String title, String author, String isbn) {
+        int Variable1 = 23423;
+        Book newBook = new Book(title, author, isbn);
+        books.add(newBook);
+        System.out.println("Added: " + newBook);
+    }
+
+    // Method to remove a book by ISBN
+    public void removeBookByIsbn(String isbn) {
+        int Variable2 = 23423;
+        books.removeIf(book -> book.getIsbn().equals(isbn));
+        System.out.println("Removed book with ISBN: " + isbn);
+    }
+
+    // Method to display all books in the library
+    public void displayBooks() {
+        if (books.isEmpty()) {
+
+            System.out.println("No books available in the library.");
+        } else {
+            System.out.println("Available books in the library:");
+            for (Book book : books) {
+                System.out.println(book);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        // Creating a Library instance
+        Library library = new Library();
+
+        // Adding books to the library
+        library.addBook("1984", "George Orwell", "12345");
+        library.addBook("To Kill a Mockingbird", "Harper Lee", "67890");
+        library.addBook("The Great Gatsby", "F. Scott Fitzgerald", "54321");
+
+        // Displaying all books
+        library.displayBooks();
+
+        // Removing a book by ISBN
+        library.removeBookByIsbn("67890");
+
+        // Displaying all books after removal
+        library.displayBooks();
+    }
+}
---
+++ src/Parser.java
@@ -0,0 +1,100 @@
+
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.expr.VariableDeclarationExpr;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
+import com.github.javaparser.ast.visitor.VoidVisitor;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+
+import java.io.FileInputStream;
+
+
+public class Parser {
+
+    public static void main(String[] args) throws Exception {
+        FileInputStream in = new FileInputStream("sample/Library.java");
+
+        CompilationUnit cu;
+        try {
+            cu = StaticJavaParser.parse(in);
+        } finally {
+            in.close();
+        }
+//        new ClassDiagramVisitor().visit(cu, null);
+//        new MethodParam().visit(cu,null);
+//        new VariableVisitor().visit(cu,null);
+
+
+        VariableVisitor variableVisitor = new VariableVisitor();
+        new MethodVar(variableVisitor).visit(cu, null);
+    }
+
+    /**
+     * Simple visitor implementation for extracting class relationship information
+     */
+//    private static class ClassDiagramVisitor extends VoidVisitorAdapter {
+//
+//        @Override
+//        public void visit(ClassOrInterfaceDeclaration n, Object arg){
+//            System.out.println("Class Name: " + n.getName());
+//
+//            System.out.println("Class Implements: ");
+//            for (ClassOrInterfaceType coi : n.getImplementedTypes()) {
+//                System.out.println(coi.getName());
+//            }
+//
+//            System.out.println("Extended: ");
+//            if(!n.getExtendedTypes().isEmpty()){
+//                System.out.println("True");
+//            }else{
+//                System.out.println("False");
+//            }
+//
+//            super.visit(n, arg);
+//        }
+//
+//        @Override
+//        public void visit(FieldDeclaration n, Object a){
+//            System.out.println("Field Type is: " + n.getElementType());
+//            for(VariableDeclarator v : n.getVariables()){
+//                System.out.println("Name: " + v.getName());
+//            }
+//        }
+//    }
+
+    private static class VariableVisitor extends VoidVisitorAdapter<Object> {
+        @Override
+        public void visit(VariableDeclarationExpr n, Object arg) {
+            for (VariableDeclarator v : n.getVariables()) {
+                System.out.println("Variable Name:" + v.getNameAsString() + " Type:" + v.getTypeAsString() + " Value:" + v.getInitializer());
+            }
+        }
+    }
+
+    private static class MethodParam extends VoidVisitorAdapter<Object> {
+        @Override
+        public void visit(MethodDeclaration n, Object arg) {
+            System.out.println("Method Name:" + n.getName() + " Parameters:" + n.getParameters());
+        }
+    }
+
+    private static class MethodVar extends VoidVisitorAdapter<Object> {
+        private final VariableVisitor variableVisitor;
+
+        public MethodVar(VariableVisitor variableVisitor) {
+            this.variableVisitor = variableVisitor;
+        }
+
+        @Override
+        public void visit(MethodDeclaration n, Object arg) {
+            System.out.println("Method Name: " + n.getNameAsString());
+            n.getBody().ifPresent(body -> {
+                body.accept(variableVisitor, arg);
+            });
+        }
+    }
+}
